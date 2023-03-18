c7db14c7fe5b07221bde6b81c8dd7420bf16a9f6
musiakam <kamil.musial@tietoevry.com>
2023-03-18 11:07:51 +0100

    2

--- 1_regresja_wielu_zmiennych.py
+++ 1_regresja_wielu_zmiennych.py
@@ -21,7 +21,35 @@ df.Height *= 2.54
 df.Weight /= 2.2
 print(df.head(10))
 
-sns.displot(df.Weight)
-plt.show()
+# sns.displot(df.Weight)
+# plt.show()
+# razem dla male i female
 
+# sns.displot(df.query("Gender=='Male'").Weight, stat='density')
+# plt.title('Male')
+# sns.displot(df.query("Gender=='Female'").Weight)
+# plt.title('Famale')
+sns.displot(df, x='Weight', hue='Gender')
+#plt.show()
+
+#gender nie jest daną numeryczną
+df = pd.get_dummies(df)   #zmiana na dane numeryczne
+print(df)
+del(df["Gender_Male"])
+print(df)
+df = df.rename(columns={"Gender_Female":"Gender"})
+print(df)
+# 1 - female,  0 - male
+
+model = LinearRegression()
+model.fit(df[["Height","Gender"]],df["Weight"])  #wejście, wynik
+print(model.coef_, model.intercept_)
+df2 = pd.DataFrame(model.coef_, ["Height","Gender"])
+print(df2)
+
+#moje dane
+gender = 1
+height = 192
+weight = model.intercept_ + model.coef_[0]*height + model.coef_[1]*gender
+print(weight)
 
