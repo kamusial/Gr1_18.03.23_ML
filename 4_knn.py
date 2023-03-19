c0a5c8c0cf59a1cf56dd28785fc30da443baa75b
musiakam <kamil.musial@tietoevry.com>
2023-03-19 10:36:32 +0100

    2

--- 4_knn.py
+++ 4_knn.py
@@ -23,14 +23,14 @@ print(df["class_value"].value_counts())
 #nowy kwiat - moj
 sample = np.array([5.6, 3.2, 5.2, 1.45])
 
-#plt.figure(figsize=(7,7))  #ustawienie wielkości wykresu
-sns.scatterplot(data=df, x='sepallength', y='sepalwidth', hue='class')
-plt.scatter(5.6, 3.2, c='r')
-#plt.show()
-
-sns.scatterplot(data=df, x='petallength', y='petalwidth', hue='class')
-plt.scatter(5.2, 1.45, c='r')
-#plt.show()
+# #plt.figure(figsize=(7,7))  #ustawienie wielkości wykresu
+# sns.scatterplot(data=df, x='sepallength', y='sepalwidth', hue='class')
+# plt.scatter(5.6, 3.2, c='r')
+# #plt.show()
+#
+# sns.scatterplot(data=df, x='petallength', y='petalwidth', hue='class')
+# plt.scatter(5.2, 1.45, c='r')
+# #plt.show()
 
 #obliczanie dystansu pomiędzy probką, a istniejącymi danymi
 df["distance"] = (df.sepallength-sample[0])**2 + (df.sepalwidth-sample[1])**2 +\
@@ -41,7 +41,7 @@ print(df.sort_values("distance").head(5)['class'].value_counts())
 
 print(df.head(5).to_string())
 X = df.iloc[:, :4]
-y = df.class_value
+y = df.class_value   #dane_wynikowe
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=0)
 #algorytm knn w wersji klasyfikatora sklearn
@@ -49,3 +49,26 @@ model = KNeighborsClassifier(5)    #liczba sąsiadów
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test)  ) ))
+
+result = []
+for k in range(1, 100):
+    model = KNeighborsClassifier(k)  # liczba sąsiadów
+    model.fit(X_train, y_train)
+    result.append(model.score(X_test, y_test))
+plt.plot(range(1,100), result)
+plt.grid()
+plt.show()
+
+#zapis i załadowanie modelu
+import joblib
+# model = KNeighborsClassifier(5)
+# model.fit(X_train, y_train)
+#model.score(X_test, y_test)
+#joblib.dump(model, 'knn.model')   #zapis modelu jako plik
+model_new = joblib.load('knn.model')
+model_new.predict(sample.reshape(1, -1))
+print(dir(model_new))
+print(model_new.n_neighbors)
+
+
+
